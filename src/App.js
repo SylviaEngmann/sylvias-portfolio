758c4e9a354c7f810db4b1b153dfabd1044e0fb4
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-04-12 01:23:42 +0000

    Intro component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import Contact from './components/Contact';
 import Socials from './components/Socials';
 import Logo from './components/Logo';
 import ProfileImage from './components/ProfileImage';
+import Intro from './components/Intro';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <a
-          className="home"
-          style={linkStyle}
-          href="#home"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          SME.
-        </a>
-      </header> */}
       <header className="App-header">
         <Logo></Logo>
       </header>
       <body>
         <Socials></Socials>
+        <Intro></Intro>
         <ProfileImage></ProfileImage>
         <Contact></Contact>
       </body>
---
+++ src/components/Intro.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+class Intro extends Component {
+    render(){
+        return(
+            <div className="intro">
+                <p>
+                Hello, I'm Sylvia. <br/>
+                A Web Designer & Full Stack Engineer, <br/>
+                based in Ghana.
+                </p>
+            </div> 
+        )
+    }
+}
+
+export default Intro;
