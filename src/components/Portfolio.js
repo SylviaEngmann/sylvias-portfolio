15e419dcbbbe710d0ef94551f8d54b5301b2a6d1
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-07-24 11:45:04 +0000

    Portfolio page changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Row, Col, Card} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import '../App.css';
+import { faCodeBranch, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const cardStyle = {
     marginTop: '1rem',
@@ -20,9 +22,29 @@ function Portfolio() {
                         <Card.Body>
                             <Card.Title style={{fontWeight:'800', fontSize: '1.5rem'}}>my health passport.</Card.Title>
                             <Card.Text>
-                            Having access to healthcare data at every point.
+                            Having access to healthcare data at every point.   
                             </Card.Text>
                             <Card.Footer>Built with React,  Node/ExpressJS and Postgres.</Card.Footer>
+                            <div className="codeLinks">
+                                <ul>
+                                    <li>
+                                        <a 
+                                        target = "_blank"
+                                        rel = "noreferrer"
+                                        href="https://github.com/SylviaEngmann/my-health-passport.">
+                                            <FontAwesomeIcon icon={faCodeBranch} />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a 
+                                        target = "_blank"
+                                        rel = "noreferrer"
+                                        href="https://myhealthpassport.herokuapp.com">
+                                            <FontAwesomeIcon icon={faGlobe} />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -38,7 +60,27 @@ function Portfolio() {
                             <Card.Text>
                             A note taking application.
                             </Card.Text>
-                            <Card.Footer>Built with React,  ExpressJS and MongoDB.</Card.Footer>
+                            <Card.Footer>Built with React,ExpressJS and MongoDB.</Card.Footer>
+                            <div className="codeLinks">
+                                <ul>
+                                    <li>
+                                        <a 
+                                        target = "_blank"
+                                        rel = "noreferrer"
+                                        href="https://github.com/SylviaEngmann/nota">
+                                            <FontAwesomeIcon icon={faCodeBranch} />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a 
+                                        target = "_blank"
+                                        rel = "noreferrer"
+                                        href="https://nota-app-notes.herokuapp.com/">
+                                            <FontAwesomeIcon icon={faGlobe} />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -50,11 +92,31 @@ function Portfolio() {
                 <Col sm={12} md lg={6}>
                     <Card style={cardStyle}>
                         <Card.Body>
-                            <Card.Title style={{fontWeight:'800', fontSize: '1.5rem'}}>Eat It Upp</Card.Title>
+                            <Card.Title style={{fontWeight:'800', fontSize: '1.5rem'}}>Eat It Up</Card.Title>
                             <Card.Text>
                             A cafeteria management system.
                             </Card.Text>
-                            <Card.Footer>Built with React,  Java and Postgres </Card.Footer>
+                            <Card.Footer>Built with React,Java and Postgres </Card.Footer>
+                            <div className="codeLinks">
+                                <ul>
+                                    <li>
+                                        <a 
+                                        target = "_blank"
+                                        rel = "noreferrer"
+                                        href="https://github.com/SylviaEngmann/eat-it-up">
+                                            <FontAwesomeIcon icon={faCodeBranch} />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a 
+                                        target = "_blank"
+                                        rel = "noreferrer"
+                                        href="https://eat-it-up.herokuapp.com/">
+                                            <FontAwesomeIcon icon={faGlobe} />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </Card.Body>
                     </Card>
                 </Col>
