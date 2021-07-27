a33d1bbaa4ae914d229e54ba7a43a9c8cbc6c1ab
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-07-27 06:40:57 +0000

    changes to about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,14 +8,23 @@ import AboutText from './AboutText';
 function About () {
 
         return(
+            <div>
                 <Row>
-                    <Col>
+                    <Col sm={0} md lg={3}>
+                    </Col>
+                    <Col sm={12} md lg={6}>
                         <AboutImage />
                     </Col>
-                    <Col>
+                    <Col sm={0} md lg={3}></Col>
+                </Row>
+                <Row>
+                    <Col sm={0} md lg={3}></Col>
+                    <Col sm={12} md lg={6}>
                         <AboutText />
                     </Col>
+                    <Col sm={0} md lg={3}></Col>
                 </Row>
+            </div>
                 
         )
     }
